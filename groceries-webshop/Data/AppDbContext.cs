75b6c21fb6300e9bd3acbaf5d23257fc6889755e
Simon Sörqvist <simon.t.sorqvist@gmail.com>
2024-04-09 17:17:12 +0200

    1 data for database create database (#2)
    * update .gitignore
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * Create product-data.json
    
    * Product model created
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * product sample data created
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * Update Product.cs
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * product sampledata ready for migration
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * Change DB name in Appsettings
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * Migration/Database now works
    
    Co-authored-by: Adam <kumlinadam99@gmail.com>
    
    ---------
    
    Co-authored-by: Adam Kumlin <kumlinadam99@gmail.com>
    Co-authored-by: Adam Kumlin <skola.adamkumlin99@gmail.com>

--- groceries-webshop/Data/AppDbContext.cs
+++ groceries-webshop/Data/AppDbContext.cs
@@ -1,12 +1,14 @@
 ﻿using groceries_webshop.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 
 namespace groceries_webshop.Data
 {
     public class AppDbContext : DbContext
     {
         public DbSet<Account> Accounts { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
---
+++ groceries-webshop/Models/Product.cs
@@ -0,0 +1,36 @@
+using System.Text.Json.Serialization;
+
+namespace groceries_webshop.Models
+{
+    public enum Category
+    {
+        Fruits,
+        Vegetables,
+        Nuts,
+        Legumes,
+        Condiments,
+        Other,
+        Berries,
+        Seeds
+    }
+
+    public class Product
+    {
+        public int ID { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("image")]
+        public string? Image { get; set; }
+
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+
+        [JsonPropertyName("category")]
+        public Category Category { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+    }
+}
