38273f263e9c3ac84b05e322741bd88f3d954552
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-04-09 17:08:37 +0200

    Migration/Database now works
    Co-authored-by: Adam <kumlinadam99@gmail.com>

--- groceries-webshop/Models/Product.cs
+++ groceries-webshop/Models/Product.cs
@@ -9,20 +9,27 @@ namespace groceries_webshop.Models
         Nuts,
         Legumes,
         Condiments,
-        Other
+        Other,
+        Berries,
+        Seeds
     }
 
     public class Product
     {
         public int ID { get; set; }
+
         [JsonPropertyName("name")]
         public string Name { get; set; }
+
         [JsonPropertyName("image")]
         public string? Image { get; set; }
+
         [JsonPropertyName("price")]
         public decimal Price { get; set; }
+
         [JsonPropertyName("category")]
         public Category Category { get; set; }
+
         [JsonPropertyName("description")]
         public string? Description { get; set; }
     }
