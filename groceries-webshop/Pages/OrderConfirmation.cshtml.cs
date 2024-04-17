a3f33764f6c6fcdc870dd8e4d5918ee403b240bc
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-04-17 09:41:41 +0100

    fix confirmation page, style pages (#7)

--- groceries-webshop/Pages/OrderConfirmation.cshtml.cs
+++ groceries-webshop/Pages/OrderConfirmation.cshtml.cs
@@ -1,24 +1,49 @@
-//using groceries_webshop.Data;
-//using groceries_webshop.Models;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
+using groceries_webshop.Data;
+using groceries_webshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-//namespace groceries_webshop.Pages
-//{
-//    public class OrderConfirmationModel : PageModel
-//    {
-//        private readonly AppDbContext _context;
+namespace groceries_webshop.Pages
+{
+    public class OrderConfirmationModel : PageModel
+    {
+        private readonly AppDbContext _context;
 
-//        public Account Account { get; set; }
+        public OrderConfirmationModel(AppDbContext context)
+        {
+            _context = context;
+        }
 
-//        public OrderConfirmationModel(AppDbContext context)
-//        {
-//            _context = context;
-//        }
+        public Account Account { get; set; }
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+        public List<Product> Products { get; set; } = new List<Product>();
 
-//        public void OnGet(int id)
-//        {
-//            Account = _context.Accounts.Find(id);
-//        }
-//    }
-//}
+        public decimal GetTotal()
+        {
+            decimal total = 0;
+
+            foreach (Product product in Products)
+            {
+                total += product.Price;
+            }
+
+            return total;
+        }
+
+        public void OnGet(int id)
+        {
+            // Get logged in account
+            Account = _context.Accounts.Find(id);
+
+            // Get account's cart items
+            CartItems = _context.CartItems.Where(c => c.AccountID == id).ToList();
+
+            // Get products from cart items
+            foreach (CartItem cartItem in CartItems)
+            {
+                Product product = _context.Products.Where(p => p.ID == cartItem.ProductID).FirstOrDefault();
+                Products.Add(product);
+            }
+        }
+    }
+}
