22d93dc9af4b144b2ef616119aaa50ef91569034
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2024-04-17 15:55:57 +0200

    working on next+previous page buttons (not done)
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>

--- groceries-webshop/Pages/Index.cshtml.cs
+++ groceries-webshop/Pages/Index.cshtml.cs
@@ -5,38 +5,63 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace groceries_webshop.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly AppDbContext _context;
-        public List<Product> Products { get; set; } = new List<Product>();
+	public class IndexModel : PageModel
+	{
+		private readonly AppDbContext _context;
 
-        public IndexModel(AppDbContext context)
-        {
-            _context = context;
-        }
+		public IndexModel(AppDbContext context)
+		{
+			_context = context;
+		}
 
-        public ActionResult OnPost(int accountID, int productID)
-        {
-            _context.CartItems.Add(
-                new CartItem
-                {
-                    AccountID = accountID,
-                    ProductID = productID
-                });
+		public List<Product> Products { get; set; } = new List<Product>();
 
-            _context.SaveChanges();
-            return RedirectToPage();
-        }
+		public int PageNr { get; set; }
 
-        public void OnGetPageNr(int pageNr)
-        {
+		public ActionResult OnPost(int accountID, int productID)
+		{
+			_context.CartItems.Add(
+				new CartItem
+				{
+					AccountID = accountID,
+					ProductID = productID
+				});
 
-        }
+			_context.SaveChanges();
+			return RedirectToPage();
+		}
 
-        public void OnGet()
-        {
-            // get products from _context
-            Products = _context.Products.ToList();
-        }
-    }
+		public void OnGetPageNr(bool nextPage, int pageNr)
+		{
+			if (pageNr == 1 && !nextPage)
+			{
+				return;
+			}
+
+			//if (Math.Round(Products.Count / 9) == pageNr && nextPage)
+			//{ 
+			
+			//}
+
+			if (nextPage)
+			{
+				Products = _context.Products
+					.Skip((pageNr - 1) * 9)
+					.Take(9)
+					.ToList();
+				PageNr = pageNr;
+			}
+			else
+			{
+
+			}
+		}
+
+		public void OnGet()
+		{
+			// get products from _context
+			Products = _context.Products.Take(9).ToList();
+			PageNr = 1;
+		}
+	}
 }
