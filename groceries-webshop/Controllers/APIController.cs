94f306913bb432ae584f5bb9c493bd26d38c1169
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-04-18 15:33:53 +0100

    Api (#12)
    * added product images
    
    * product list page first version
    
    * Minor changes | QoL
    
    * Display 9 products on home page
    
    * add to cart now works | shopping cart displays nr of units
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * basic css layout for list of products
    
    * working on next+previous page buttons (not done)
    
    Co-Authored-By: Adam Kumlin <kumlinadam99@gmail.com>
    
    * start creating api
    
    * add new class
    
    * implement page querying
    
    Co-Authored-By: Simon Sörqvist <simon.t.sorqvist@gmail.com>
    
    * edit querying system to rely on Contains method
    
    * finish api
    
    Co-Authored-By: Simon Sörqvist <simon.t.sorqvist@gmail.com>
    
    ---------
    
    Co-authored-by: simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
    Co-authored-by: Simon Sörqvist <simon.t.sorqvist@gmail.com>

--- groceries-webshop/Controllers/APIController.cs
+++ groceries-webshop/Controllers/APIController.cs
@@ -2,27 +2,122 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using groceries_webshop.Models;
 
 namespace groceries_webshop.Controllers
 {
-    [Route("/api")]
-    [ApiController]
-    public class APIController : ControllerBase
-    {
-        private readonly AppDbContext database;
+	[Route("/api")]
+	[ApiController]
+	public class APIController : ControllerBase
+	{
+		private readonly AppDbContext _context;
 
-        public APIController(AppDbContext database)
-        {
-            this.database = database;
-        }
+		public APIController(AppDbContext context)
+		{
+			_context = context;
+		}
 
-        [HttpGet("/products")]
-        public IEnumerable<Models.Product> GetProducts([FromQuery] string? name, [FromQuery] string? category) 
-        {
+		// sub-class only used for the api results
+		public class ProductWithImage : Product
+		{
+			public string Image { get; set; }
+		}
 
-            IEnumerable<Models.Product> products = new List<Models.Product>();
+		[HttpGet("/products")]
+		public List<ProductWithImage> GetProducts(
+			[FromQuery] string? name,
+			[FromQuery] string? categoryString,
+			[FromQuery] int page)
+		{
+			List<Product> productsWithoutImage = new List<Product>();
+			List<ProductWithImage> products = new List<ProductWithImage>();
 
-            return products;
-        }
-    }
+			// dictionary to map the string from the query to a category,
+			// so that the user of the api get images based on pre-defined categories
+			Dictionary<string, Category> categoryMap = new Dictionary<string, Category>
+			{
+				{ "Fruits", Category.Fruits },
+				{ "Vegetables", Category.Vegetables },
+				{ "Nuts", Category.Nuts },
+				{ "Legumes", Category.Legumes },
+				{ "Condiments", Category.Condiments },
+				{ "Other", Category.Other },
+				{ "Berries", Category.Berries },
+				{ "Seeds", Category.Seeds }
+			};
+
+			Category? category = null;
+			if (categoryString != null)
+			{
+				// If dictionary contains value, set category to value in dictionary, prevents an exception
+				if (categoryMap.TryGetValue(categoryString, out _))
+				{
+					category = categoryMap[categoryString];
+				}
+
+			}
+
+			// if page query is 0, return first ten posts
+			page = (page < 1) ? 1 : page;
+
+			const int resultsPerPage = 10;
+			int postsToSkip = resultsPerPage * (page - 1);
+
+			// If both name and category are specified, filter first by name, then by category
+			if (name != null && category != null)
+			{
+				productsWithoutImage = _context.Products
+					.Where(p => p.Name.Contains(name))
+					.Where(p => p.Category == category)
+					.Skip(postsToSkip)
+					.Take(resultsPerPage)
+					.ToList();
+			}
+			// filter response based on name query (contains for broader search)
+			else if (name != null)
+			{
+				productsWithoutImage = _context.Products
+					.Where(p => p.Name.Contains(name))
+					.Skip(postsToSkip)
+					.Take(resultsPerPage)
+					.ToList();
+			}
+			// filter by category (assigned based on categoryString)
+			else if (category != null)
+			{
+				productsWithoutImage = _context.Products
+					.Where(p => p.Category == category)
+					.Skip(postsToSkip)
+					.Take(resultsPerPage)
+					.ToList();
+			}
+			else
+			{
+				productsWithoutImage = _context.Products
+					.Skip(postsToSkip)
+					.Take(resultsPerPage)
+					.ToList();
+			}
+
+			foreach (Product product in productsWithoutImage)
+			{
+				// append image url to products returned by api
+				ProductWithImage newProduct = new ProductWithImage
+				{
+					ID = product.ID,
+					Name = product.Name,
+					Price = product.Price,
+					Category = product.Category,
+					Description = product.Description,
+					Image = "https://localhost:5000/images/products/" +
+						product.Name.ToLower().Replace(" ", "") +
+						".jpg"
+				};
+
+				products.Add(newProduct);
+			}
+
+			return products;
+		}
+	}
 }
