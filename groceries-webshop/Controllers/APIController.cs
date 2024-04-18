6ff79e7a3ef0605a7ef22298f714eb2a6d76a803
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-04-18 14:02:34 +0200

    implement page querying
    Co-Authored-By: Simon Sörqvist <simon.t.sorqvist@gmail.com>

--- groceries-webshop/Controllers/APIController.cs
+++ groceries-webshop/Controllers/APIController.cs
@@ -6,40 +6,67 @@ using groceries_webshop.Models;
 
 namespace groceries_webshop.Controllers
 {
-    [Route("/api")]
-    [ApiController]
-    public class APIController : ControllerBase
-    {
-        private readonly AppDbContext _database;
+	[Route("/api")]
+	[ApiController]
+	public class APIController : ControllerBase
+	{
+		private readonly AppDbContext _database;
 
-        public APIController(AppDbContext database)
-        {
-            _database = database;
-        }
+		public APIController(AppDbContext database)
+		{
+			_database = database;
+		}
+
+		public class ProductWithImage : Product
+		{
+			public string Image { get; set; }
+		}
 
 		[HttpGet("/products")]
-        public List<ProductWithImage> GetProducts([FromQuery] string? name, [FromQuery] Category? category) 
-        {
-
+		public List<ProductWithImage> GetProducts([FromQuery] string? name, [FromQuery] Category? category, [FromQuery] int page)
+		{
 			List<Product> productsWithoutImage = new List<Product>();
-            List<ProductWithImage> products = new List<ProductWithImage>();
+			List<ProductWithImage> products = new List<ProductWithImage>();
+
+			// if page query is 0, return first ten posts
+			page = (page < 1) ? 1 : page;
+
+			const int resultsPerPage = 10;
+			int postsToSkip = resultsPerPage * (page - 1);
 
 			// If both name and category are specified, filter first by name, then by category
 			if (name != null && category != null)
 			{
-				productsWithoutImage = _database.Products.Where(p => p.Name == name).Where(p => p.Category.Equals(category)).ToList();
+				productsWithoutImage = _database.Products
+					.Where(p => p.Name == name)
+					.Where(p => p.Category
+					.Equals(category))
+					.Skip(postsToSkip)
+					.Take(resultsPerPage)
+					.ToList();
 			}
 			else if (name != null)
 			{
-				productsWithoutImage = _database.Products.Where(p => p.Name == name).ToList();
+				productsWithoutImage = _database.Products
+					.Where(p => p.Name == name)
+					.Skip(postsToSkip)
+					.Take(resultsPerPage)
+					.ToList();
 			}
 			else if (category != null)
 			{
-				productsWithoutImage = _database.Products.Where(p => p.Category.Equals(category)).ToList();
+				productsWithoutImage = _database.Products
+					.Where(p => p.Category.Equals(category))
+					.Skip(postsToSkip)
+					.Take(resultsPerPage)
+					.ToList();
 			}
 			else
 			{
-				productsWithoutImage = _database.Products.ToList();
+				productsWithoutImage = _database.Products
+					.Skip(postsToSkip)
+					.Take(resultsPerPage)
+					.ToList();
 			}
 
 			foreach (Product product in productsWithoutImage)
@@ -51,22 +78,15 @@ namespace groceries_webshop.Controllers
 					Price = product.Price,
 					Category = product.Category,
 					Description = product.Description,
-					Image = "https://localhost:5001/images/products/" + product.Name.ToLower().Replace(" ", "") + ".jpg"
+					Image = "https://localhost:5000/images/products/" +
+						product.Name.ToLower().Replace(" ", "") + 
+						".jpg"
 				};
 
 				products.Add(newProduct);
 			}
 
 			return products;
-        }
-
-        [HttpGet("/products")]
-        public IEnumerable<Models.Product> GetProducts([FromQuery] string? name, [FromQuery] string? category) 
-        {
-
-            IEnumerable<Models.Product> products = new List<Models.Product>();
-
-            return products;
-        }
-    }
+		}
+	}
 }
